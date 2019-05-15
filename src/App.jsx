d96ed5a1483eba589207ee3d376a661b29f097e1
tagivis <tagivis@hotmail.com>
2019-05-15 11:59:58 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+// import PersonalInfo from './PersonalInfo/PersonalInfo';
+// import MyHobbies from './MyHobbies/MyHobbies';
+// import MyBooks from './MyBooks/MyBooks';
+
+export default class App extends Component {
+  render() {
+    return (
+    //   <div class="appStyle">
+    //     <PersonalInfo/>
+    //     <div class="divider"></div>
+    //     <MyHobbies/>
+    //     <div class="divider"></div>
+    //     <MyBooks/>
+    //   </div>
+    )
+  }
+}
